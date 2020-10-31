355618dbf068b8a6226bf04ec0951cfb1e7e71d4
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-10-31 20:53:49 +0700

    Buoi 7
    Menu Window form

---
+++ UngDung1/WindowsFormsApp1/formPhuongTrinh/formPhuongTrinhBac1.cs
@@ -0,0 +1,54 @@
+﻿using SuDungHam;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.formPhuongTrinh
+{
+    public partial class formPhuongTrinhBac1 : Form
+    {
+        public formPhuongTrinhBac1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            try
+            {
+                if (double.TryParse(txtSoA.Text, out a) == false){
+                    txtSoA.SelectAll();
+                    txtSoA.Focus();
+                    throw new Exception("Số A Nhập Không Hợp Lệ");
+                }
+                if (double.TryParse(txtSoA.Text, out b) == false)
+                {
+                    txtSoB.SelectAll();
+                    txtSoB.Focus();
+                    throw new Exception("Số B Nhập Không Hợp Lệ");
+                }
+                PhuongTrinh pt = new PhuongTrinh();
+                
+                txtTong.Text = 
+                    pt.PhuongTrinhBac1(a, b).ToString();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Thông Báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+    }
+}
